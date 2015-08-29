5297ad4969be190120ed8e426b9a2797c52cfaa8
David Wen <dstwen@gmail.com>
2015-08-29 11:35:24 -0700

    Refactor landing controller

--- app/assets/javascripts/angular/controllers/landing_controller.js
+++ app/assets/javascripts/angular/controllers/landing_controller.js
@@ -6,74 +6,119 @@ app.controller('LandingCtrl', ['$scope', function($scope) {
   for (var i = 0; i < sliders.length; i++) {
     var sliderHolder = $(sliders[i]).parents(".spec-content-holder")
     var spec = specDetails(sliderHolder)
+    var detailsForSlider = sliderDetails(spec, spec.rangePrecision)
 
-    var steps = (spec.rangeMax - spec.rangeMin) / spec.rangeInterval
-    var stepsToDisplay = 5
-    var decimalUse = null
-    var decimals = 0
+    if (detailsForSlider.steps > 10) {
+      createSlider(sliders[i], spec, detailsForSlider, "count")
+    } else {
+      createSlider(sliders[i], spec, detailsForSlider, "steps")
+    }
+
+    initiateSliderUpdate(sliders[i])
+  };
+
+  $scope.dropdownSelected = function($event) {
+    var ele = $($event.target)
+    var selectedText = ele.text()
+    ele.parents(".slider-dropdown").find(".button-text").text(selectedText)
+
+    var sliderHolder = ele.parents(".spec-content-holder")
+    var spec = specDetails(sliderHolder)
+    var slider = sliderHolder.find(".slider")
+
+    slider.after("<div class='slider'></div>")
+    var newSlider = slider.siblings(".slider")[0]
+    slider.remove()
+
+    var dualRangeText = "Within (Dual range)"
+
+    if (selectedText == dualRangeText) {
+      var detailsForSlider = sliderDetails(spec, dualRangeText)
+      createSlider(newSlider, spec, detailsForSlider, "count")
+      var textArea = sliderHolder.find(".selected-text")
+      textArea.text(initialRangeTextWithUom(detailsForSlider.start, spec))
+    } else {
+
+    }
+    initiateSliderUpdate(newSlider)
+  }
+
+  function initialRangeTextWithUom(start, spec) {
+    return "From: " + start[0] + " " + spec.uomAbbreviation + " to " + start[1] + " " + spec.uomAbbreviation
+  }
+
+  function roundWithUOM(value) {
+    valueArr = value.split(" ")
+    return Math.round(valueArr[0]) + " " + valueArr[1]
+  }
+
+  function numSteps(spec) {
+    return (spec.rangeMax - spec.rangeMin) / spec.rangeInterval
+  }
+
+  function specDetails(slider) {
+    var spec = {}
+    spec["unitType"]          = slider.attr("data-unit-type")
+    spec["rangeMin"]          = parseInt(slider.attr("data-range-min"))
+    spec["rangeMax"]          = parseInt(slider.attr("data-range-max"))
+    spec["rangeInterval"]     = parseInt(slider.attr("data-range-interval"))
+    spec["rangeDefault"]      = parseInt(slider.attr("data-range-default"))
+    spec["rangeDefaultMulti"] = parseInt(slider.attr("data-range-default-multi"))
+    spec["rangePrecision"]    = slider.attr("data-range-default-precision")
+    spec["fractionBase"]      = parseInt(slider.attr("data-fraction-base"))
+    spec["uomAbbreviation"]   = slider.attr("data-uom-abbreviation")
+    return spec
+  }
+
+  function sliderDetails(spec, rangePrecision) {
+    var details = {}
+    details["steps"] = numSteps(spec)
+    details["stepsToDisplay"] = 5
+
     if (spec.unitType == "Decimal") {
-      decimalUse = true
-      decimals = 1
-    } else if (spec.unitType == "Fraction") {
-      stepsToDisplay = 5
+      details["decimalUse"] = true
+      details["decimals"] = 1
     } else {
-      decimalUse = false
+      details["decimalUse"] = false
+      details["decimals"] = 0
     }
 
-    var start = null
-    var connect = null
-    if (spec.rangePrecision == "Within (Dual range)") {
-      start = [spec.rangeDefault, spec.rangeDefaultMulti]
-      connect = true
+    if (rangePrecision == "Within (Dual range)") {
+      details["start"] = [spec.rangeDefault, spec.rangeDefaultMulti]
+      details["connect"] = true
     } else {
-      start = spec.rangeDefault
-      connect = 'lower'
+      details["start"] = spec.rangeDefault
+      details["connect"] = 'lower'
     }
 
-    if (steps > 10) {
-      noUiSlider.create(sliders[i], {
-        start: start,
-        decimals: decimalUse,
-        step: spec.rangeInterval,
-        connect: connect,
-        range: {
-          'min':  spec.rangeMin,
-          'max':  spec.rangeMax
-        },
-        pips: {
-          mode: 'count',
-          values: stepsToDisplay,
-          density: 100,
-          stepped: true
-        },
-        format: wNumb({
-          decimals: decimals,
-          postfix: ' ' + spec.uomAbbreviation
-        })
-      });
-    } else {
-      noUiSlider.create(sliders[i], {
-        start: start,
-        decimals: decimalUse,
-        step: spec.rangeInterval,
-        connect: connect,
-        range: {
-          'min':  spec.rangeMin,
-          'max':  spec.rangeMax
-        },
-        pips: {
-          mode: 'steps',
-          density: 100,
-          stepped: true
-        },
-        format: wNumb({
-          decimals: decimals,
-          postfix: ' ' + spec.uomAbbreviation
-        })
-      });
-    }
+    return details
+  }
 
-    sliders[i].noUiSlider.on('update', function(values, handle){
+  function createSlider(slider, spec, detailsForSlider, mode) {
+    noUiSlider.create(slider, {
+      start: detailsForSlider.start,
+      decimals: detailsForSlider.decimalUse,
+      step: spec.rangeInterval,
+      connect: detailsForSlider.connect,
+      range: {
+        'min':  spec.rangeMin,
+        'max':  spec.rangeMax
+      },
+      pips: {
+        mode: mode,
+        values: detailsForSlider.stepsToDisplay,
+        density: 100,
+        stepped: true
+      },
+      format: wNumb({
+        decimals: detailsForSlider.decimals,
+        postfix: ' ' + spec.uomAbbreviation
+      })
+    });
+  }
+
+  function initiateSliderUpdate(slider) {
+    slider.noUiSlider.on('update', function(values, handle){
       var parentEle = $(event.target).parents(".spec-content-holder")
       var unitType = parentEle.attr("data-unit-type")
       var value1 = null
@@ -97,58 +142,5 @@ app.controller('LandingCtrl', ['$scope', function($scope) {
         parentEle.find(".selected-text").text(value1)
       }
     });
-  };
-
-  $scope.dropdownSelected = function($event) {
-    var ele = $($event.target)
-    var eleParent = ele.parents(".spec-content-holder")
-    var sliderHolder = eleParent.find(".slider")
-    var spec = specDetails(sliderHolder)
-
-    var selectedText = ele.text()
-    ele.parents(".slider-dropdown").find(".button-text").text(selectedText)
-
-    if (spec.defaultPrecision == "Within (Dual range)") {
-      noUiSlider.create(sliders[i], {
-        start: start,
-        decimals: decimalUse,
-        step: spec.rangeInterval,
-        connect: connect,
-        range: {
-          'min':  spec.rangeMin,
-          'max':  spec.rangeMax
-        },
-        pips: {
-          mode: 'steps',
-          density: 100,
-          stepped: true
-        },
-        format: wNumb({
-          decimals: decimals,
-          postfix: ' ' + spec.uomAbbreviation
-        })
-      });
-    } else {
-
-    }
-  }
-
-  function roundWithUOM(value) {
-    valueArr = value.split(" ")
-    return Math.round(valueArr[0]) + " " + valueArr[1]
-  }
-
-  function specDetails(slider) {
-    var spec = {}
-    spec["unitType"]          = slider.attr("data-unit-type")
-    spec["rangeMin"]          = parseInt(slider.attr("data-range-min"))
-    spec["rangeMax"]          = parseInt(slider.attr("data-range-max"))
-    spec["rangeInterval"]     = parseInt(slider.attr("data-range-interval"))
-    spec["rangeDefault"]      = parseInt(slider.attr("data-range-default"))
-    spec["rangeDefaultMulti"] = parseInt(slider.attr("data-range-default-multi"))
-    spec["rangePrecision"]    = slider.attr("data-range-default-precision")
-    spec["fractionBase"]      = parseInt(slider.attr("data-fraction-base"))
-    spec["uomAbbreviation"]   = slider.attr("data-uom-abbreviation")
-    return spec
   }
 }]);
