979e47700e02d79bc21307899bfc289d968a202f
David Wen <dstwen@gmail.com>
2015-08-30 12:34:44 -0700

    Fix side menu showing bug

--- app/assets/javascripts/angular/controllers/side_bar_nav_controller.js
+++ app/assets/javascripts/angular/controllers/side_bar_nav_controller.js
@@ -6,11 +6,24 @@ app.controller('SideBarNavCtrl', ['$scope', function($scope) {
 
   $("#vehicle-accordion").on('show.bs.collapse', function(event) {
     var majorSectionId = $(event.target).siblings(".panel-heading").attr("id")
+    var currentlyOpen = $(event.target).hasClass("in")
     $(".major-section-contents").hide()
-    $("." + majorSectionId).slideDown("fast")
+    if (!currentlyOpen) {
+      $("." + majorSectionId).slideDown("fast")
+    }
+    // $('html, body').animate({ scrollTop: 0 }, 'fast');
   })
 
-  $("#vehicle-accordion").on('hide.bs.collapse', function(event) {
-    $(".major-section-contents").slideUp("fast")
+  $("body").on("click", ".panel-heading", function(event) {
+    var ele = $(event.target)
+    var parent = ele
+    if (ele.hasClass("panel-title")) {
+      parent = ele.parents(".panel-heading")
+    }
+    var majorSectionId = parent.attr("id")
+    var sibling = parent.siblings(".panel-collapse")
+    if (sibling.hasClass("in")) {
+      $(".side-nav-holder").find("." + majorSectionId).slideUp("fast")
+    }
   })
 }]);
